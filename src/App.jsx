0235253175c9c6a2f7773c511d44c5d5e3e7e7e8
JoelVR17 <vermudo.com@gmail.com>
2022-11-30 11:58:21 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1 `
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+`
+
+function App() {
+
+  return (
+    <Heading>Hola</Heading>
+  )
+}
+
+export default App
